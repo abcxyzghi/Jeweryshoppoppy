251f694f0780814aced3298ca38d2755e2f3803b
Phan Quang Khai <khaipqse183063@fpt.edu.vn>
2024-07-04 15:59:50 +0700

    Update AuthenticationRepository

--- Jewelry-Store/src/main/java/online/jewerystorepoppy/be/repository/AuthenticationRepository.java
+++ Jewelry-Store/src/main/java/online/jewerystorepoppy/be/repository/AuthenticationRepository.java
@@ -1,13 +1,21 @@
 package online.jewerystorepoppy.be.repository;
 
+import online.jewerystorepoppy.be.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import online.jewerystorepoppy.be.entity.Account;
+
+import java.util.List;
 
 public interface AuthenticationRepository extends JpaRepository<Account, Long> {
 
     Account findAccountByPhone(String phone);
 
     Account findAccountByEmail(String email);
+    Account findAccountById(long id);
 
     Account findAccountByEmailOrPhone(String email, String phone);
+
+    List<Account> findAccountByEmailContainingOrPhoneContainingOrFullNameContaining(String email, String phone, String fullName);
+
+    List<Account> findAccountByRole(Role role);
 }
