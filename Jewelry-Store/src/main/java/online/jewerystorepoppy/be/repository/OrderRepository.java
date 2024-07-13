474f9f5251f9328fbaad449062d87d2493e2e7ff
Ghost-3107 <khiabeo2@gmail.com>
2024-07-13 18:32:10 +0700

    AddOrderApi

--- Jewelry-Store/src/main/java/online/jewerystorepoppy/be/repository/OrderRepository.java
+++ Jewelry-Store/src/main/java/online/jewerystorepoppy/be/repository/OrderRepository.java
@@ -3,5 +3,8 @@ package online.jewerystorepoppy.be.repository;
 import online.jewerystorepoppy.be.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Orders, Long> {
+    List<Orders> findOrdersByCustomerPhoneContaining(String phone);
 }
