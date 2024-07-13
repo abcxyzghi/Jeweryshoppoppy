b429ffd76d4eca7440b661c1cf807b05eb793ab0
Phan Quang Khai <khaipqse183063@fpt.edu.vn>
2024-07-13 15:41:12 +0700

    update VoucherRepository

--- Jewelry-Store/src/main/java/online/jewerystorepoppy/be/repository/VoucherRepository.java
+++ Jewelry-Store/src/main/java/online/jewerystorepoppy/be/repository/VoucherRepository.java
@@ -1,10 +1,15 @@
 package online.jewerystorepoppy.be.repository;
 
+import online.jewerystorepoppy.be.entity.Product;
 import online.jewerystorepoppy.be.entity.Voucher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
 public interface VoucherRepository extends JpaRepository<Voucher, Long> {
     List<Voucher> findVouchersByIsDeletedFalse();
+    List<Voucher> findVoucherByCode(String code);
+    Voucher findVoucherById(long id);
 }
