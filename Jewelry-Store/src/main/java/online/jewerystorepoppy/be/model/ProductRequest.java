b754bdb6931b66a1030c41a010599f8b980f54c2
Phan Quang Khai <khaipqse183063@fpt.edu.vn>
2024-07-24 21:13:16 +0700

    update ProductRequest

--- Jewelry-Store/src/main/java/online/jewerystorepoppy/be/model/ProductRequest.java
+++ Jewelry-Store/src/main/java/online/jewerystorepoppy/be/model/ProductRequest.java
@@ -2,10 +2,18 @@ package online.jewerystorepoppy.be.model;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class ProductRequest {
     String name;
     String description;
     float price;
     long categoryId;
+    int quantity;
+    String code;
+    String image;
+    List<Long> materialIds = new ArrayList<>();
+    List<Long> sizeIds;
 }
