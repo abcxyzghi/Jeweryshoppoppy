46efe1cc16bfb9f00d885ae50f2c4b93f9789faa
soilangtu <134293705+soilangtu@users.noreply.github.com>
2024-07-26 00:00:24 +0700

    Delete front end file directory

--- front end file/src/pages/utils/currency.js
+++
@@ -1,7 +0,0 @@
-export default function convertCurrency(number) {
-  // Use the Intl.NumberFormat object to format the number as Vietnamese currency
-  return new Intl.NumberFormat("vi-VN", {
-    style: "currency",
-    currency: "VND",
-  }).format(number);
-}
