03c2695799871a8e23554672ef9a53c4050e6e51
Ghost-3107 <khiabeo2@gmail.com>
2024-07-26 07:44:58 +0700

    updateOrder

--- Jewelry-Store/src/main/java/online/jewerystorepoppy/be/entity/Orders.java
+++ Jewelry-Store/src/main/java/online/jewerystorepoppy/be/entity/Orders.java
@@ -1,18 +1,19 @@
 package online.jewerystorepoppy.be.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import online.jewerystorepoppy.be.enums.OrderStatus;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
-@ToString
 public class Orders {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,18 +23,25 @@ public class Orders {
     float totalAmount;
     OrderStatus status;
     String description;
+    int point = 0;
 
     @ManyToOne
     @JoinColumn(name = "account_id")
     Account createBy;
 
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    Account customer;
+
     @OneToMany(mappedBy = "order")
-    List<Voucher> vouchers;
+    @JsonIgnore
+    List<Voucher> vouchers = new ArrayList<>();
 
     @ManyToOne()
+    @JsonIgnore
     @JoinColumn(name = "shift_id")
     Shift shift;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     List<OrderDetail> orderDetails;
 }
