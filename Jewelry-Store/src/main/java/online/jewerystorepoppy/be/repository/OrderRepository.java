59d8f51534b8eaf6f7b83c781354d2e836362d83
Ghost-3107 <khiabeo2@gmail.com>
2024-07-26 07:51:25 +0700

    updateOrder

--- Jewelry-Store/src/main/java/online/jewerystorepoppy/be/repository/OrderRepository.java
+++ Jewelry-Store/src/main/java/online/jewerystorepoppy/be/repository/OrderRepository.java
@@ -2,6 +2,17 @@ package online.jewerystorepoppy.be.repository;
 
 import online.jewerystorepoppy.be.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
+    List<Orders> findOrdersByCustomerPhoneContaining(String phone);
+
+    @Query("SELECT o FROM Orders o WHERE o.createBy.id = :id AND FUNCTION('MONTH', o.createAt) = :month AND FUNCTION('YEAR', o.createAt) = :year")
+    List<Orders> findOrdersByMonthAndYear(@Param("month") int month, @Param("year") int year, @Param("id") long accountId);
+
+    @Query("SELECT o FROM Orders o WHERE  FUNCTION('MONTH', o.createAt) = :month AND FUNCTION('YEAR', o.createAt) = :year")
+    List<Orders> findOrdersByMonthAndYear(@Param("month") int month, @Param("year") int year);
 }
